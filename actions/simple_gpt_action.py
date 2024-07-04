970ab65ab005ea407b4e2a4b6e7e5c7bd5fca328
Jørgen Sandhaug <jorgen.sandhaug@gmail.com>
2024-07-04 23:53:28 +0200

    feat: add groq models

--- actions/simple_gpt_action.py
+++ actions/simple_gpt_action.py
@@ -1,5 +1,5 @@
 from actions.BaseAction import BaseAction
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage, SystemMessage, AIMessage
 import pyperclip
 from prompts.prompts import system_prompt_with_input, system_prompt_default
@@ -11,17 +11,30 @@ from config.config import Config
 cfg = Config()
 
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_groq import ChatGroq
 
-class Models(Enum):
+
+import os
+
+class Model(Enum):
     GPT3 = "gpt-3.5-turbo"
     GPT4 = "gpt-4"
+    GPT4o = "gpt-4o"
+    GROQ_LLAMA_3_8B = "llama3-8b-8192"
+    GROQ_LLAMA_3_70B = "llama3-70b-8192"
+    GROQ_MIXTRAL = "mistral-7b-32768"
 
 last_chat = None
 
-def simple_gpt_action(input_text, model_name = Models.GPT4, system_prompt=system_prompt_with_input, followup=False):
+def simple_gpt_action(input_text, model_name = Model.GPT4, system_prompt=system_prompt_with_input, followup=False):
     global last_chat
 
-    chat = ChatOpenAI(model_name=model_name.value)
+    if model_name.value.startswith("llama"):
+        chat = ChatGroq(model_name=model_name.value)
+    elif model_name.value.startswith("mistral"):
+        chat = ChatGroq(model_name=model_name.value)
+    else:
+        chat = ChatOpenAI(model_name=model_name.value)
     chat.request_timeout = 180
 
     new_msg = HumanMessage(content=input_text)
@@ -45,14 +58,14 @@ def simple_gpt_action(input_text, model_name = Models.GPT4, system_prompt=system
     return response.content
 
 class SimpleGPTActionWithInput(BaseAction):
-    def __init__(self, shortcut, whisper_mode="transcribe", model_name: Models = Models.GPT3, followup=False):
+    def __init__(self, shortcut, whisper_mode="transcribe", model_name: Model = Model.GPT3, followup=False):
         self.model_name = model_name
         self.followup = followup
 
         super().__init__(
-            name="simple_gpt4_action",
+            name=f"simple_{model_name.value}_action_with_input",
             action=self.action,
-            description="Simple GPT4 Action",
+            description=f"Simple {model_name.value} Action with Input",
             shortcut=shortcut,
             config={
                 "whisper_mode": whisper_mode,
@@ -71,11 +84,11 @@ class SimpleGPTActionWithInput(BaseAction):
         return simple_gpt_action(prompt, model_name=self.model_name, followup=self.followup, system_prompt=system_prompt_with_input)
 
 class SimpleGPTAction(BaseAction):
-    def __init__(self, shortcut, model_name: Models = Models.GPT4, followup=False):
+    def __init__(self, shortcut, model_name: Model = Model.GPT4, followup=False):
         super().__init__(
-            name="simple_gpt4_action",
+            name=f"simple_{model_name.value}_action_no_input",
             action=lambda inp: simple_gpt_action(inp, model_name=model_name, followup=followup, system_prompt=cfg.system_prompt),
-            description="Simple GPT Action",
+            description=f"Simple {model_name.value} Action",
             shortcut=shortcut,
             config={
                 "whisper_mode": "transcribe",
--- config/actions_config.py
+++ config/actions_config.py
@@ -1,17 +1,21 @@
 import logging
+from actions.simple_gpt_action import SimpleGPTActionWithInput, Model, SimpleGPTAction
 from actions.translate import WhisperTranslate
 from actions.transcribe import WhisperTranscribe
 
-from config.shortcuts import hotkey_translate, hotkey_transcribe, super_key
+from config.shortcuts import hotkey_translate, hotkey_transcribe, super_key, hotkey_gpt_with_input, hotkey_gpt_no_input
 
 logger = logging.getLogger()
 
 actions = [
     WhisperTranslate(shortcut=hotkey_translate),
     WhisperTranscribe(shortcut=hotkey_transcribe),
-    # SimpleGPTActionWithInput(
-    #     shortcut=super_key | {keyboard.KeyCode.from_char("3")}, whisper_mode="transcribe", model_name=Models.GPT3
-    # ),
+    SimpleGPTActionWithInput(
+        shortcut=hotkey_gpt_with_input, whisper_mode="transcribe", model_name=Model.GROQ_LLAMA_3_8B
+    ),
+    SimpleGPTAction(
+        shortcut=hotkey_gpt_no_input, model_name=Model.GROQ_LLAMA_3_70B
+    ),
     # SimpleGPTActionWithInput(
     #     shortcut=super_key | {keyboard.KeyCode.from_char("4")}, whisper_mode="transcribe", model_name=Models.GPT4
     # ),
